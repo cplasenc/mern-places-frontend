3b4b63c3663784b3a4b828f26055a82e490be2f4
C. Plasencia <36932780+ctnplasencia@users.noreply.github.com>
2020-08-12 23:04:46 +0100

    barra de menu

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Users from './pages/Users';
 import NewPlace from './pages/Place';
-
+import MainNavigation from './components/MainNavigation/MainNavigation';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path='/' exact> <Users /> </Route>
-        <Route path='/places/new' exact> <NewPlace /> </Route>
-        <Redirect to='/' />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path='/' exact> <Users /> </Route>
+          <Route path='/places/new' exact> <NewPlace /> </Route>
+          <Redirect to='/' />
+        </Switch>
+      </main>
     </BrowserRouter>
 
   );
--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './UsersList.css'
 import UserItem from '../UserItem/UserItem';
+import Card from '../UIElements/Card/Card';
 
 const UsersList = props => {
     if (props.items.length === 0) {
         return (
             <div className='center'>
-                <h2>Usuarios no encontrados</h2>
+                <Card>
+                    <h2>Usuarios no encontrados</h2>
+                </Card>
             </div>
         );
     }
