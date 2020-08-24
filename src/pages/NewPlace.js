133709608a2326288f4e86764aefb101999e02ad
C. Plasencia <36932780+ctnplasencia@users.noreply.github.com>
2020-08-25 00:20:12 +0100

    validaciones para el formulario

--- src/pages/NewPlace.js
+++ src/pages/NewPlace.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './NewPlace.css';
+import Input from '../components/Input/Input';
+import { VALIDATOR_REQUIRE } from '../util/validators';
 
 const NewPlace = () => {
-    return <h2>NewPlace</h2>
+    return (
+        <form className='place-form'>
+            <Input element='input' type='text' label='Title' validators={[VALIDATOR_REQUIRE()]} errorText='Introduce un valor válido' />
+        </form>
+    )
 };
 
 export default NewPlace;
