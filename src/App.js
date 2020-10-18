287ab4f8b1b1cd89ece9e1ab77289f2f7ce79160
C. Plasencia <36932780+ctnplasencia@users.noreply.github.com>
2020-10-18 19:37:33 +0100

    auth context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Users from './pages/Users';
@@ -7,23 +7,36 @@ import UpdatePlace from './pages/UpdatePlace';
 import MainNavigation from './components/MainNavigation/MainNavigation';
 import UserPlaces from './pages/UserPlaces';
 import Auth from './pages/Auth/Auth';
+import { AuthContext } from './context/auth-context';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path='/' exact> <Users /> </Route>
-          <Route path='/:userId/places' exact> <UserPlaces /> </Route>
-          <Route path='/places/new' exact> <NewPlace /> </Route>
-          <Route path='/places/:placeId'> <UpdatePlace /> </Route>
-          <Route path='/auth'> <Auth /> </Route>
-          <Redirect to='/' />
-        </Switch>
-      </main>
-    </BrowserRouter>
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}>
+      <BrowserRouter>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path='/' exact> <Users /> </Route>
+            <Route path='/:userId/places' exact> <UserPlaces /> </Route>
+            <Route path='/places/new' exact> <NewPlace /> </Route>
+            <Route path='/places/:placeId'> <UpdatePlace /> </Route>
+            <Route path='/auth'> <Auth /> </Route>
+            <Redirect to='/' />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
--- src/components/NavLinks/NavLinks.js
+++ src/components/NavLinks/NavLinks.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './NavLinks.css';
 import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../../context/auth-context';
 
 const NavLinks = props => {
+
+    const auth = useContext(AuthContext);
+
     return <ul className='nav-links'>
         <li>
             <NavLink to='/'>ALL USERS</NavLink>
         </li>
-        <li>
-            <NavLink to='/u1/places'>MY PLACES</NavLink>
-        </li>
-        <li>
-            <NavLink to='/places/new'>ADD PLACE</NavLink>
-        </li>
-        <li>
-            <NavLink to='/auth'>LOGIN</NavLink>
-        </li>
+        {auth.isLoggedIn && (
+            <li>
+                <NavLink to='/u1/places'>MY PLACES</NavLink>
+            </li>
+        )}
+        {auth.isLoggedIn && (
+            <li>
+                <NavLink to='/places/new'>ADD PLACE</NavLink>
+            </li>
+        )}
+        {!auth.isLoggedIn && (
+            <li>
+                <NavLink to='/auth'>LOGIN</NavLink>
+            </li>
+        )}
     </ul>
 };
 
