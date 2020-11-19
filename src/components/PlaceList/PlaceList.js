46dd5a5bde220fe76b763ad52c081288e90c55c6
C. Plasencia <36932780+ctnplasencia@users.noreply.github.com>
2020-11-19 19:59:56 +0000

    cargar por id de usuario (error)

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -16,9 +16,21 @@ const PlaceList = props => {
         );
     }
 
-    return <ul className='place-list'>
-        {props.items.map(place => <PlaceItem key={place.id} id={place.id} image={place.imageUrl} title={place.title} description={place.description} address={place.address} creatorId={place.creator} coordinates={place.location} />)}
-    </ul>;
+    return (<ul className='place-list'>
+        {props.items.map(place => (
+            <PlaceItem 
+                key={place.id} 
+                id={place.id} 
+                image={place.image} 
+                title={place.title} 
+                description={place.description} 
+                address={place.address} 
+                creatorId={place.creator} 
+                coordinates={place.location} 
+            />
+        ))}
+    </ul>
+    );
 };
 
 export default PlaceList;
--- src/pages/UserPlaces.js
+++ src/pages/UserPlaces.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PlaceList from '../components/PlaceList/PlaceList';
 import { useParams } from 'react-router-dom';
-
-const DUMMY_PLACES = [
-    {
-        id: '1',
-        title: 'titulo',
-        description: 'descripcion',
-        imageUrl: 'https://s3-us-west-2.amazonaws.com/lasaga-blog/media/original_images/grupo_imagen.jpg',
-        address: 'direccion de prueba',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u1'
-    },
-    {
-        id: '2',
-        title: 'titulo2',
-        description: 'descripcion',
-        imageUrl: 'https://s3-us-west-2.amazonaws.com/lasaga-blog/media/original_images/grupo_imagen.jpg',
-        address: 'direccion de prueba',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u2'
-    }
-];
+import { useHttpClient } from '../hooks/http-hook';
+import ErrorModal from '../components/UIElements/ErrorModal/ErrorModal';
+import Spinner from '../components/UIElements/Spinner/LoadingSpinner';
+import LoadingSpinner from '../components/UIElements/Spinner/LoadingSpinner';
 
 const UserPlaces = () => {
-    //consigue el id de usuario de la url
+    const [loadedPlaces, setLoadedPlaces] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
     const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
 
-    return <PlaceList items={loadedPlaces} />
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try {
+                const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+                setLoadedPlaces(responseData.places);
+            } catch (error) {
+                
+            }
+        };
+        fetchPlaces();
+    }, [sendRequest, userId]);
 
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && (
+                <div className='center'>
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+            <PlaceList items={loadedPlaces} />
+        </React.Fragment>
+    );
 };
 
 export default UserPlaces;
