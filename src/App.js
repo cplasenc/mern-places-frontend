65f03d24ae1a95d4eff66e9c77df6d548be17932
Cristian Plasencia <plc.cristian@gmail.com>
2021-01-09 23:48:52 +0000

    custom hook para autentificación

--- src/App.js
+++ src/App.js
@@ -8,53 +8,11 @@ import MainNavigation from './components/MainNavigation/MainNavigation';
 import UserPlaces from './pages/UserPlaces';
 import { AuthContext } from './context/auth-context';
 import Auth from './pages/Auth/Auth';
-
-let logoutTimer;
+import {useAuth } from './hooks/auth-hook';
 
 const App = () => {
 
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(false);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token);
-    setUserId(uid);
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000*60*60);
-    setTokenExpirationDate(tokenExpirationDate);
-    localStorage.setItem('userData', JSON.stringify({
-      userId: uid, token: 
-      token, 
-      expiration: tokenExpirationDate.toISOString() 
-    }));
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExpirationDate(null);
-    setUserId(null);
-    localStorage.removeItem('userData');
-  }, []);
-
-  useEffect(() => {
-    if(token && tokenExpirationDate) {
-      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpirationDate]);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if(
-      storedData && 
-      storedData.token && 
-      new Date(storedData.expiration) > new Date()
-      ) {
-      login(storedData.userId, storedData.token, new Date(storedData.expiration));
-    }
-  }, [login]);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
