8792681491287da281f9ea9347415fe8fad3292e
Cristian Plasencia <plc.cristian@gmail.com>
2021-01-09 19:40:26 +0000

    auto login y auto logout implementacion

--- src/App.js
+++ src/App.js
@@ -9,29 +9,41 @@ import UserPlaces from './pages/UserPlaces';
 import { AuthContext } from './context/auth-context';
 import Auth from './pages/Auth/Auth';
 
+let logoutTimer;
+
 const App = () => {
 
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    const tokenExpirationData = expirationDate || new Date(new Date().getTime() + 1000*60*60);
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000*60*60);
+    setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem('userData', JSON.stringify({
       userId: uid, token: 
       token, 
-      expiration: tokenExpirationData.toISOString() 
+      expiration: tokenExpirationDate.toISOString() 
     }));
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
     localStorage.removeItem('userData');
   }, []);
 
-  let routes;
+  useEffect(() => {
+    if(token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('userData'));
@@ -43,6 +55,8 @@ const App = () => {
       login(storedData.userId, storedData.token, new Date(storedData.expiration));
     }
   }, [login]);
+
+  let routes;
 
   if (token) {
     routes = (
